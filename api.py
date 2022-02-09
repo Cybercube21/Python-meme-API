67080f2959959016a252cfa3d21459105d44079a
cybercube21 <cybercube@mailbox.org>
2022-02-09 11:59:44 +0100

    Changed api.py

--- api.py
+++ api.py
@@ -2,15 +2,16 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
+import os, random
 
 app = Flask(__name__)
 api = Api(app)
+file_list=os.listdir(r"./")
 
 class Memes(Resource):
     def get(self):
-        data = pd.read_csv('users.csv')  # read local CSV
-        data = data.to_dict()  # convert dataframe to dict
-        return {'data': data}, 200  # return data and 200 OK
+        meme = random.choice(file_list)
+        return {'url': 'https://api.cybercube21.de/memes/' + meme}, 200  # return data and 200 OK
 
 api.add_resource(Memes, '/memes')
 
