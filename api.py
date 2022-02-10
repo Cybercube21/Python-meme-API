bfe6f7084f550c9f599bc4e89264ba5b0b8b6df8
cybercube21 <cybercube@mailbox.org>
2022-02-10 17:15:18 +0100

    Added Count File & Calculation

--- api.py
+++ api.py
@@ -14,13 +14,29 @@ file_list=os.listdir(r"memes/")
 def index():
     return render_template("index.html")
 
+def update_counter():
+    # Get Count & add 1
+    f = open("count.txt", "r")
+    api_calls_counter = (f.read())
+    api_calls_counter = int(api_calls_counter) + 1
+    lines = f.readlines()
+    f.close()
+
+    #Remove old count & write new count to file
+    f = open("count.txt", "w")
+    for line in lines:
+        f.write(line)
+    f.write(str(api_calls_counter)) 
+    f.close()
+
 class Ping(Resource):
     def get(self):
+        update_counter()
         return {'respone': 'your mom','error': 'none','code': 200}
-
 
 class Memes(Resource):
     def get(self):
+        update_counter()
         meme = random.choice(file_list)
         return {'url': 'https://api.cybercube21.de/memes/' + meme}, 200  # return data and 200 OK
 
