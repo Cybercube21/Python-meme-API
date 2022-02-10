2f3bb43acf2697dae7a7fce717e0317671b6207f
cybercube21 <cybercube@mailbox.org>
2022-02-10 15:06:45 +0100

    Changed Memes / Added Ping

--- api.py
+++ api.py
@@ -14,11 +14,17 @@ file_list=os.listdir(r"memes/")
 def index():
     return render_template("index.html")
 
+class Ping(Resource):
+    def get(self):
+        return {'respone': 'your mom','error': 'none','code': 200}
+
+
 class Memes(Resource):
     def get(self):
         meme = random.choice(file_list)
         return {'url': 'https://api.cybercube21.de/memes/' + meme}, 200  # return data and 200 OK
 
+api.add_resource(Ping, '/ping')
 api.add_resource(Memes, '/memes')
 
 if __name__ == '__main__':
