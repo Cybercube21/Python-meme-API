b2487df97d79a0b39231ab173a2f5c59fdc7e82c
cybercube21 <cybercube@mailbox.org>
2022-02-11 12:53:34 +0100

    Added thread for memes folder update

--- api.py
+++ api.py
@@ -1,14 +1,35 @@
+from email.errors import UndecodableBytesDefect
 from flask import Flask
 from flask_restful import Resource, Api
 from flask import render_template
 import os, random
+import threading
+import inotify.adapters
 
 app = Flask(__name__,
             static_url_path='', 
             static_folder='./',
             template_folder='templates/')
 api = Api(app)
+
 file_list=os.listdir(r"memes/")
+
+notify_adapter = inotify.adapters.Inotify()
+notify_adapter.add_watch('memes/')
+
+def watch_dir():
+    global file_list
+    for event in notify_adapter.event_gen(yield_nones=False):
+        (_, type_names, path, filename) = event
+        print(type_names)
+
+        if (('IN_CLOSE_WRITE' in type_names or 'IN_MOVED_TO' in type_names) and filename != ''):
+            file_list=os.listdir(r"memes/")
+            print("New File")
+
+
+watch_thread = threading.Thread(target=watch_dir)
+watch_thread.start()
 
 @app.route("/")
 def index():
@@ -45,3 +66,4 @@ api.add_resource(Memes, '/memes')
 
 if __name__ == '__main__':
     app.run(port=6969)
+    watch_thread.join()
